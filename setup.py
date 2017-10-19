2b3d17a34b3a4792acb416e13c5c14adc03725b1
zhexiao <zhexiao@163.com>
2017-10-19 08:16:31 +0000

    1.2.2

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """A setuptools based setup module.
 
 See:
@@ -18,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='audep',
-    version='1.2.1',
+    version='1.2.2',
     description='自动化部署平台',
     long_description=long_description,
 
